b1479678e60bbdc7085714f95f2415d89276b4f7
Fedi NAIMI <62253740+fedinaimi@users.noreply.github.com>
2025-10-17 12:32:47 -0400

    refactor: Improve organization and readability of SimpleFloatingDockNav component

--- app/components/SimpleFloatingDockNav.tsx
+++ app/components/SimpleFloatingDockNav.tsx
@@ -1,12 +1,12 @@
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import {
-  Dimensions,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    Dimensions,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
